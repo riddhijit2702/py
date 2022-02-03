17255d1c199e9b370a0fc6387b57a9be6dd4ef2b
spaceCrazy <74096278+riddhijit2702@users.noreply.github.com>
2022-02-03 17:55:42 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,62 @@
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import statistics
+import random
+import pandas as pd
+
+df = pd.read_csv("sadtruth.csv")
+data = df["claps"].tolist()
+
+mean = statistics.mean(data)
+std_deviation = statistics.stdev(data)
+print("mean of popultion:- ",mean)
+print("Standard deviation of popultion:- ",std_deviation)
+
+
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index= random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+
+
+# Pass the number of time you want the mean of the data points as a parameter in range function in for loop
+mean_list = []
+for i in range(0,100):
+    set_of_means= random_set_of_mean(30)
+    mean_list.append(set_of_means)
+
+
+## calculating mean and standard_deviation of the sampling distribution.
+std_deviation = statistics.stdev(mean_list)
+
+
+
+first_std_deviation_start, first_std_deviation_end = mean-std_deviation, mean+std_deviation
+second_std_deviation_start, second_std_deviation_end = mean-(2*std_deviation), mean+(2*std_deviation)
+third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mean+(3*std_deviation)
+
+fig = ff.create_distplot([mean_list], ["claps"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1 END"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2 END"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_end, third_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 3 END"))
+fig.show()
+
+df = pd.read_csv("sadtruth.csv")
+data = df["claps"].tolist()
+mean_of_sample1 = statistics.mean(data)
+print("Mean of sample1:- ",mean_of_sample1)
+fig = ff.create_distplot([mean_list], ["claps"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1 END"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2 END"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_end, third_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 3 END"))
+fig.show()
+
+z_score = (mean - mean_of_sample1)/std_deviation
+print("The z score is = ",z_score)
